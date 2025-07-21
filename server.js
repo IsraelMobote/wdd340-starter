c184971868f81756648dcd22d9fce0b8534ae780
IsraelMobote <moboteisrael02@gmail.com>
2025-07-21 16:55:27 +0100

    Added the items in the inventory views dynamically

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /* ***********************
@@ -28,6 +29,9 @@ app.use(static)
 
 // Index route
 app.get("/", baseController.buildHome)
+
+//Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
