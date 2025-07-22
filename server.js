511c9720d5c1faab3af35bbc079c30bdcac3715f
IsraelMobote <moboteisrael02@gmail.com>
2025-07-23 00:26:23 +0100

    edit

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(async (req, res, next) => {
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
+  let nav = await Util.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
   if (err.status == 404) {
     message = err.message
