783222c26dbc2f5c80c1810a1849bd89387f7317
IsraelMobote <moboteisrael02@gmail.com>
2025-07-31 05:47:32 +0100

    edit

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -9,7 +9,7 @@ async function buildRegister(req, res, next) {
     res.render("account/register", {
         title: "Register",
         nav,
-        errors: null
+        errors: null,
     })
 }
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,11 +2,15 @@ const express = require("express")
 const router = new express.Router()
 const util = require("../utilities/index")
 const acctController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 router.get("/login", util.handleErrors(acctController.buildLogin));
 router.get("/register", util.handleErrors(acctController.buildRegister));
 
 //route for the post method in the register view form
-router.post("/register", util.handleErrors(acctController.registerAccount));
+router.post("/register", 
+    regValidate.registationRules(),
+    regValidate.checkRegData, 
+    util.handleErrors(acctController.registerAccount));
 
 module.exports = router;
